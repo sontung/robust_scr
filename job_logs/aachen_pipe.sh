a413ed0cfda780913aef7dd0bbb18007d9123266
sontung <n11373598@QUT-PA00154617.qut.edu.au>
2025-11-15 18:43:16 +1000

    2

--- job_logs/aachen_pipe.sh
+++ job_logs/aachen_pipe.sh
@@ -17,4 +17,4 @@ cd /home/n11373598/work/robust_scr
   exit 1
 }
 
-#/home/n11373598/.pixi/bin/pixi run python -u ace_trainer_with_loftr2.py  --scene $SCENE_PATH --global_feat 1 --use_aug 1 --iter_output 1000000 --depth_target 12 --feat_name checkpoints/desc_aachen0.npy  --feat_name_test checkpoints/desc_aachen0_test.npy   --training_buffer_size 128000000 --max_iterations 100000 --batch_size 320000 --graph_aug 1 --use_half 1 --focus_tune 1 --output_map_file head_main.pth
+/home/n11373598/.pixi/bin/pixi run python trainer_with_aachen.py  --scene $SCENE_PATH --global_feat 1 --use_aug 1 --iter_output 1000000 --depth_target 12 --feat_name checkpoints/desc_aachen0.npy  --feat_name_test checkpoints/desc_aachen0_test.npy   --training_buffer_size 128000000 --max_iterations 100000 --batch_size 320000 --graph_aug 1 --use_half 1 --focus_tune 1 --output_map_file head_main.pth
