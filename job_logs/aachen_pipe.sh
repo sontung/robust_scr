32561c72584c1c9e6358466f2aa1d9e8bf7e24f6
v.tungns30 <v.tungns30@vinrobotics.net>
2026-03-04 08:15:56 +0700

    test

--- job_logs/aachen_pipe.sh
+++ job_logs/aachen_pipe.sh
@@ -18,6 +18,6 @@ cd /home/tungns30/robust_scr
 #/home/tungns30/.pixi/bin/pixi run scr-overlap-score --data $SCENE_PATH/train --max_depth 50
 #/home/tungns30/.pixi/bin/pixi run scr-encoding-pca dedode --encoder.detector L --encoder.descriptor B --n_components 128 --data $SCENE_PATH
 
-/home/tungns30/.pixi/bin/pixi run python train_agg_aachen.py $SCENE_PATH 0 desc_aachen0 64 10000 0 16 1e-3 9e-10 1 2 --margin_t 0.9
+/home/tungns30/.pixi/bin/pixi run python train_agg_aachen.py $SCENE_PATH 0 desc_aachen0 64 10000 0 16 1e-3 9e-10 1 0 --margin_t 0.9
 
 /home/tungns30/.pixi/bin/pixi run python trainer_with_aachen.py  --scene $SCENE_PATH --global_feat 1 --use_aug 1 --iter_output 1000000 --depth_target 12 --feat_name checkpoints/desc_aachen0.npy  --feat_name_test checkpoints/desc_aachen0_test.npy   --training_buffer_size 128000000 --max_iterations 100000 --batch_size 320000 --graph_aug 1 --use_half 1 --focus_tune 1 --output_map_file head_main.pth
